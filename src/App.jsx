2270ccf2bc6e4b53129753efd31b59d5de1d73b4
Ali Amin <prof.aliamin@gmail.com>
2024-07-29 13:21:15 +0300

    loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [Loading, setLoading] = useState(true);
+	const [Recipes, setRecipes] = useState([]);
+	//function to fetch all recipes:
+	const fetchRecipes = async () => {
+		try {
+			const response = await fetch("https://dummyjson.com/recipes?limit=6");
+			const data = await response.json();
+			setRecipes(data.recipes);
+			setLoading(false);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
+	useEffect(() => {
+		fetchRecipes();
+	}, []);
 	return (
-		<>
-			<h1>Hi</h1>
-		</>
+		<main>
+			<div className="table-container">
+				<table>
+					<thead>
+						<tr>
+							<th>Recipe Name</th>
+							<th>Ingredients</th>
+							<th>Instructions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{!Loading ? (
+							Recipes.map((recipes) => (
+								<tr key={recipes.id}>
+									<td>{recipes.name}</td>
+									<td> {recipes.ingredients.join(", ")}</td>
+									<td> {recipes.instructions.join(" ")} </td>
+								</tr>
+							))
+						) : (
+							<tr>
+								<td>Fetching Data</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+			</div>
+		</main>
 	);
 }
 
